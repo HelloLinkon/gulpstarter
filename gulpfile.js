0b3e3a18bd29b2eae195bde882668fd6f7bb1d98
hellolinkon <linkondhk@gmail.com>
2015-11-18 02:42:47 +0600

    add gulp plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,71 @@
 // /////////////////////////////////////////
 
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload(),
+	compass = require('gulp-compass'),
+	rename = require('gulp-rename');
 
 // /////////////////////////////////////////
 // Scripts Task
 // /////////////////////////////////////////
 gulp.task('scripts', function(){
-	console.log('it works');
+	gulp.src(['app/js/**/*.js', '!app/js/**/*.min.js'])
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'));
+});
+
+// /////////////////////////////////////////
+// style Task
+// /////////////////////////////////////////
+gulp.task('compass',function(){
+	gulp.src('app/scss/style.scss')
+		.pipe(compass({
+			config_file: './config.rb',
+			css: 'app/css',
+			sass: 'app/scss',
+			require: ['susy']
+		}))
+		.pipe(gulp.dest('app/css'));
+		
+});
+
+// /////////////////////////////////////////
+// html Task
+// /////////////////////////////////////////
+gulp.task('html', function(){
+	gulp.src('app/**/*.html');
+});
+
+
+// /////////////////////////////////////////
+// browserSync Task
+// /////////////////////////////////////////
+gulp.task('browser-sync', function(){
+	browserSync({
+		server:{
+			baseDir: "./app/"
+		}
+	});
+
+});
+
+
+
+
+// /////////////////////////////////////////
+// Watch Task
+// /////////////////////////////////////////
+gulp.task('watch', function(){
+	gulp.watch('app/js/**/*.js', ['scripts']);
+	gulp.watch('app/scss/**/*.scss', ['compass']);
+	gulp.watch('app/**/*.html', ['html']);
 });
 
 
 // /////////////////////////////////////////
 // Default Task
 // /////////////////////////////////////////
-gulp.task('default', ['scripts']);
+gulp.task('default', ['scripts', 'html', 'compass','browser-sync','watch' ]);
